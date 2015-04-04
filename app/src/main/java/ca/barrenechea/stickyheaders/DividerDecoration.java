425506e42da46fb5f01fd95f3da771cd98ae7433
Eduardo Barrenechea <eduardo.barrenechea@gmail.com>
2015-04-04 09:30:13 -0400

    Add divider decoration to help with debugging.

---
+++ app/src/main/java/ca/barrenechea/stickyheaders/DividerDecoration.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2014 Eduardo Barrenechea
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.barrenechea.stickyheaders;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.View;
+
+public class DividerDecoration extends RecyclerView.ItemDecoration {
+
+    private int mHeight;
+    private Paint mPaint;
+
+    public DividerDecoration(Context context) {
+        mHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, 1f, context.getResources().getDisplayMetrics());
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int count = parent.getChildCount();
+
+        for (int i = 0; i < count; i++) {
+            final View child = parent.getChildAt(i);
+            final int top = child.getTop();
+            final int bottom = top + mHeight;
+            final int left = child.getLeft();
+            final int right = child.getRight();
+
+            c.save();
+            c.drawRect(left, top, right, bottom, mPaint);
+            c.restore();
+        }
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        outRect.set(0, 0, 0, mHeight);
+    }
+}
